da727a6d67953af46c2d6503f96b32a59c85127c
SunsetFi <sunsetfidev@gmail.com>
2023-08-30 10:58:51 -0700

    Character api

---
+++ src/api/mixins/character.ts
@@ -0,0 +1,24 @@
+import { GameEventTiming } from "../../types/events.js";
+import { GameSpeed } from "../../types/time.js";
+import { RESTApiBase } from "../RESTApiBase.js";
+
+import { ConstructorOf } from "../types-internal.js";
+
+export interface CharacterSHMixin {
+  getUniqueManifestedElements(): Promise<string[]>;
+  getRecipeExecutions(): Promise<Record<string, number>>;
+}
+
+export function CharacterSHMixin<C extends ConstructorOf<RESTApiBase>>(
+  constructor: C
+) {
+  return class extends constructor implements CharacterSHMixin {
+    getUniqueManifestedElements(): Promise<string[]> {
+      return this.request("GET", `/character/manifested-elements`);
+    }
+
+    getRecipeExecutions(): Promise<Record<string, number>> {
+      return this.request("GET", `/character/recipes-executed`);
+    }
+  };
+}
